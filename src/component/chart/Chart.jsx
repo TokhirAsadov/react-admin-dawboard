beea52dd3290e2fbbff19191927abcf426c36d03
TokhirAsadov <86370041+TokhirAsadov@users.noreply.github.com>
2022-03-08 07:02:03 +0500

    chart created and finished

---
+++ src/component/chart/Chart.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+import "./Chart.css"
+import {ResponsiveContainer, LineChart, XAxis, YAxis, Line, Tooltip, CartesianGrid} from "recharts";
+
+export default function Chart({ title, data, dataKey, grid }){
+
+    return(
+        <div className="chart">
+            <h3 className="chartTitle">{title}</h3>
+            <ResponsiveContainer width="100%" aspect={4 / 1}>
+                <LineChart data={data}>
+                    <XAxis dataKey="name" stroke="grey"/> {/* dataKey - OX ni belgilaydi( x ning qiymatlari ). stroke - kordetaning chiziq ranggi*/}
+                    <YAxis /> {/* OY - o`qi */}
+                    <Line type="monotone" dataKey={dataKey} stroke="grey"/>
+                    {/* Line - grafik chizadi. dataKey - buyicha tasvirlaydi( y ning qiymatlari )  */}
+                    <Tooltip /> {/* grafikka hover qilinganida ma'lumotlarni kursatishni taminlaydi */}
+                    { grid && <CartesianGrid stroke={"#e0adff"} strokeDasharray="5 5"/> }
+                    {"kvadrat katakchalarga bulib beradi, strokeDasharray uzuq uzuq chiziq bilan chizadi kvadratlarni"}
+                </LineChart>
+            </ResponsiveContainer>
+        </div>
+    );
+}
