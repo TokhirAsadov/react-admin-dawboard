c2fbc4dc8e777a45742b687f53ceeaba55baf904
TokhirAsadov <86370041+TokhirAsadov@users.noreply.github.com>
2022-04-01 08:51:25 +0500

    add new /product link.

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import './App.css';
 import Topbar from "./component/topbar/Topbar";
 import Sidebar from "./component/sidebar/Sidebar";
 import Home from "./pages/home/Home";
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom"
+import UserList from "./component/userList/UserList";
+import Product from "./pages/product/Product"
+
 
 function App() {
   return (
-    <div>
-        <Topbar />
-        <div className="container">
-              <Sidebar />
-              <Home />
-        </div>
-    </div>
+  <Router>
+      <Topbar />
+      <div className="container">
+          <Sidebar />
+          <Routes>
+              <Route path={"/"} exact element={<Home />} />
+              <Route path={"/users"} element={<UserList />}/>
+              <Route path={"/product"} element={<Product />} />
+          </Routes>
+      </div>
+  </Router>
+
   );
 }
 
